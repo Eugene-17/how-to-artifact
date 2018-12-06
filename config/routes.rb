4891f7df11db191370bdb89683b72a5f3f68cb3c
BinhDV <dangbinh1996914@gmail.com>
2018-12-06 23:25:32 +0700

    add css, static home and about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :posts
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   resources :categories
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'about', to: 'about#about'
   root to: 'home#index'
 end
