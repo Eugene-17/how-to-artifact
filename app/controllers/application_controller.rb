9acf2a1d89b527a3ff9cf184c85f584cc5943947
BinhDV <dangbinh1996914@gmail.com>
2018-12-11 08:54:29 +0700

    moving

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,8 +26,8 @@ class ApplicationController < ActionController::Base
 
   protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password) }
-      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:nickname,:firstname,:lastname, :email, :password, :current_password, :gender,:birthday,:description,:avatar) }
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password) }
+      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:nickname,:firstname,:lastname, :email, :password, :current_password, :gender,:birthday,:description,:avatar) }
     end
 
 end
--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,2 +1,8 @@
 class NewsController < ApplicationController
+  def index
+    @news = Post.order(:created_at).reverse_order.page(params[:page])
+    @top_article = Post.where(featured: true).order(:updated_at).first
+    @news_image = PostImage.all
+    @author = User.all
+  end
 end
