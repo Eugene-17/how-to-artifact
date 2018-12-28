cb5ae602722cbd80edbded136eb547551d5d5e62
BinhDV <dangbinh1996914@gmail.com>
2018-12-28 17:03:02 +0700

    fix homepage, add user show

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,13 +3,18 @@ class HomeController < ApplicationController
     @posts = Post.order(:created_at).reverse_order
     @top_post = Post.where(featured: true).order(:updated_at).first
 
-    if(Post.where(featured: true, category_id: 2).order(:updated_at).count >= 2 )
-      @top_news = Post.where(featured: true, category_id: 2).order(:updated_at).second
-      @top_guide = Post.where(featured: true, category_id: 2).order(:updated_at).first
+    if(Post.where(featured: true, category_id: 1).order(:updated_at).count >= 1 )
+      @top_news = Post.where(featured: true, category_id: 1).order(:updated_at).reverse_order.first
     else
-      @top_news = Post.where(category_id: 2).order(:updated_at).second
-      @top_guide = Post.where(category_id: 2).order(:updated_at).first
+      @top_news = Post.where(category_id: 1).order(:updated_at).reverse_order.first
     end
+
+    if(Post.where(featured: true, category_id: 2).order(:updated_at).count >= 1 )
+      @top_guide = Post.where(featured: true, category_id: 2).order(:updated_at).reverse_order.first
+    else
+      @top_guide = Post.where(category_id: 2).order(:updated_at).reverse_order.first
+    end
+
     @author = User.all
     @post_images  = PostImage.all
   end
