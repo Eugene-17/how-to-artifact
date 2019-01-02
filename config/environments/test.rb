b1565f33d0741c85621c70534c5372f9864fc9cc
BinhDV <dangbinh1996914@gmail.com>
2019-01-02 12:11:44 +0700

    moving

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -42,5 +42,15 @@ Rails.application.configure do
   config.active_support.deprecation = :stderr
 
   # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
+  # config.action_view.raise_on_missing_translations =
+  config.paperclip_defaults = {
+      :storage => :s3,
+      :s3_region => ENV['AWS_REGION'],
+      :s3_credentials => {
+          :bucket => ENV['S3_BUCKET_NAME'],
+          :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+          :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      }
+  }
+
 end
